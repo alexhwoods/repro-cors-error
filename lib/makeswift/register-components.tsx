f52776ded78f0d3059dbc2d6e75c6a0a56c7e9fd
Alex Woods <alexhwoods@gmail.com>
2022-10-25 16:55:02 -0400

    feat: make call to endpoint

--- lib/makeswift/register-components.tsx
+++ lib/makeswift/register-components.tsx
@@ -1,16 +1,24 @@
-import { Style } from '@makeswift/runtime/controls'
-import { ReactRuntime } from '@makeswift/runtime/react'
+import { Style } from "@makeswift/runtime/controls";
+import { ReactRuntime } from "@makeswift/runtime/react";
+import { useEffect } from "react";
 
 // Register your components here!
 
 function HelloWorld(props: { className?: string }) {
-  return <p {...props}>Hello, world!</p>
+  const x = useEffect(() => {
+    const y = fetch("https://eoyvdiswa21sakm.m.pipedream.net", {
+      headers: { "x-alex-foo": "bar" },
+    });
+
+    console.log(`y: ${JSON.stringify(y, null, 2)}`);
+  });
+  return <p {...props}>Hello, world!</p>;
 }
 
 ReactRuntime.registerComponent(HelloWorld, {
-  type: 'hello-world',
-  label: 'Hello, world!',
+  type: "hello-world",
+  label: "Hello, world!",
   props: {
     className: Style({ properties: Style.All }),
   },
-})
+});
